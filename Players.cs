431e97e2e7e2a3ae59bc91d4fe61f4569017bc16
dulgarianla <dulgarianla17@gmail.com>
2022-12-09 14:39:38 -0700

    error in Player

---
+++ Players.cs
@@ -0,0 +1,27 @@
+using Raylib_cs;
+using System.Numerics;
+
+class Players: GameObject{
+
+    public Players(){
+    }
+    
+    public override void Move(){
+        
+    }
+    
+
+}
+
+class Player1 : Players{
+
+    public Player1(){
+
+    }
+}
+
+class Player2: Players{
+      public Player2(){
+
+    }
+}
